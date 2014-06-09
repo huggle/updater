bd7e06f942e62af9059bef010d7f24419e917817
se4598 <se4598@gmx.de>
2014-06-09 20:27:15 +0200

    formatting

--- index.php
+++ index.php
@@ -12,105 +12,95 @@ class client
     {
         return "3.0.1";
     }
-    
+
     public static function LatestBeta()
     {
-    	return "3.0.2";
+    	return "3.0.2b1";
     }
-    
 
     function Failed()
     {
         return $this->failed;
     }
-    
+
     private function setError($e)
     {
-    	$this->errmsg = "<error>ERROR: $e</error>\n";
-    	$this->failed = true;
+        $this->errmsg = "<error>ERROR: $e</error>\n";
+        $this->failed = true;
     }
-    
+
     function getErrorMsg()
     {
-    	return $this->errmsg;
+        return $this->errmsg;
     }
-    
+
     function IsObsolete()
     {
-        return version_compare ( $this->client_version , $this->getNewVersion() , '<' );
+        return version_compare($this->client_version, $this->getNewVersion(), '<');
     }
-    
+
     function getNewVersion()
     {
-    	/* here can you change the logic to return the latest version based on e.g. os */
-    	if($this->beta){
-    		return self::LatestBeta();
-    	} else {
-    		return self::Latest();
-    	}
+        // here can you change the logic to return the latest version based on e.g. os
+        if ($this->beta) {
+            return self::LatestBeta();
+        } else {
+            return self::Latest();
+        }
     }
 
     function __construct()
     {
-        if (!isset($_GET['os']))
-        {
-			$this->setError("System must be defined");
+        if (!isset($_GET['os'])) {
+            $this->setError("System must be defined");
             return;
         }
-        if (!isset($_GET['version']))
-        {
+        if (!isset($_GET['version'])) {
             $this->setError("Version must be defined");
             return;
         }
-        $this->client_version = preg_replace('/[^a-zA-Z0-9-_\.]/','', $_GET['version']);
-        $this->client_os = preg_replace('/[^a-zA-Z0-9-_\.]/','', $_GET['os']);
-        
-        if(isset($_GET['notifybeta']))
-        {
-        	$this->beta = true;
+        $this->client_version = preg_replace('/[^a-zA-Z0-9-_\.]/', '', $_GET['version']);
+        $this->client_os = preg_replace('/[^a-zA-Z0-9-_\.]/', '', $_GET['os']);
+
+        if (isset($_GET['notifybeta'])) {
+            $this->beta = true;
         }
     }
 }
 
-$c = new client;
+$c = new client();
 
 header("Content-type: text/xml");
 echo "<?xml version=\"1.0\"?>\n";
 echo "<update>\n";
-if (!$c->Failed())
-{
-    if ($c->IsObsolete())
-    {
-        echo "<obsolete>" . $c->getNewVersion() . "</obsolete>\n"; // TODO: migrate away from that
+if (!$c->Failed()) {
+    if ($c->IsObsolete()) {
+        // TODO: migrate away from "obsolete"
+        echo "<obsolete>" . $c->getNewVersion() . "</obsolete>\n";
         echo "<newversion>" . $c->getNewVersion() . "</newversion>\n";
-        if($c->beta){
-        	include ("includes/beta.xml");
+        if ($c->beta) {
+            include ("includes/beta.xml");
         } else {
-	        // let's check a definition for this system
-	        $file = "includes/" . $c->client_os . "_" . $c->client_version . ".xml";
-	        if (file_exists($file))
-	        {
-	            include ($file);
-	        } else
-	        {
-	            $file = "includes/" . "none_" . $c->client_version . ".xml";
-	            if (file_exists($file))
-	            {
-	                include ($file);
-	            } else if (file_exists("includes/unknown.xml"))
-	            {
-	                include ("includes/unknown.xml");
-	            } else
-	            {
-	                echo "<error>No data for your version</error>\n";
-	            }
-	        }
+            // let's check a definition for this system
+            $file = "includes/" . $c->client_os . "_" . $c->client_version . ".xml";
+            if (file_exists($file)) {
+                include ($file);
+            } else {
+                $file = "includes/" . "none_" . $c->client_version . ".xml";
+                if (file_exists($file)) {
+                    include ($file);
+                } else if (file_exists("includes/unknown.xml")) {
+                        include ("includes/unknown.xml");
+                    } else {
+                        echo "<error>No data for your version</error>\n";
+                    }
+            }
         }
     } else {
-    	echo "<nonewversion />\n";
+        echo "<nonewversion />\n";
     }
 } else {
-	echo $c->getErrorMsg();
+    echo $c->getErrorMsg();
 }
 echo "</update>";
 
