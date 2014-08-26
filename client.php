cbfa4dfb22f559bdde796df1b9236db2bcded890
Petr Bena <benapetr@gmail.com>
2014-08-26 14:03:50 +0200

    some updates

---
+++ client.php
@@ -0,0 +1,81 @@
+<?
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+class client
+{
+    public $client_os = 'unknown';
+    public $client_version = 'unknown';
+    public $beta = false;
+    private $failed = false;
+    private $errmsg = "";
+
+    public static function Latest()
+    {
+        return "3.1.0";
+    }
+
+    public static function LatestBeta()
+    {
+    	return self::Latest(); // no beta atm
+    }
+
+    function Failed()
+    {
+        return $this->failed;
+    }
+
+    private function setError($e)
+    {
+        $this->errmsg = "<error>ERROR: $e</error>\n";
+        $this->failed = true;
+    }
+
+    function getErrorMsg()
+    {
+        return $this->errmsg;
+    }
+
+    function IsObsolete()
+    {
+        if ($this->client_os == "huggle-devs")
+            return false;
+        return version_compare($this->client_version, $this->getNewVersion(), '<');
+    }
+
+    function getNewVersion()
+    {
+        // here can you change the logic to return the latest version based on e.g. os
+        if ($this->beta) {
+            return self::LatestBeta();
+        } else {
+            return self::Latest();
+        }
+    }
+
+    function __construct()
+    {
+        if (!isset($_GET['os'])) {
+            $this->setError("System must be defined");
+            return;
+        }
+        if (!isset($_GET['version'])) {
+            $this->setError("Version must be defined");
+            return;
+        }
+        $this->client_version = preg_replace('/[^a-zA-Z0-9-_\.]/', '', $_GET['version']);
+        $this->client_os = preg_replace('/[^a-zA-Z0-9-_\.]/', '', $_GET['os']);
+
+        if (isset($_GET['notifybeta'])) {
+            $this->beta = true;
+        }
+    }
+}
+
--- index.php
+++ index.php
@@ -1,76 +1,30 @@
 <?php
 
-class client
-{
-    public $client_os = 'unknown';
-    public $client_version = 'unknown';
-    public $beta = false;
-    private $failed = false;
-    private $errmsg = "";
+// This is a core file for huggle updater
 
-    public static function Latest()
-    {
-        return "3.1.0";
-    }
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
 
-    public static function LatestBeta()
-    {
-    	return self::Latest(); // no beta atm
-    }
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
 
-    function Failed()
-    {
-        return $this->failed;
-    }
-
-    private function setError($e)
-    {
-        $this->errmsg = "<error>ERROR: $e</error>\n";
-        $this->failed = true;
-    }
-
-    function getErrorMsg()
-    {
-        return $this->errmsg;
-    }
-
-    function IsObsolete()
-    {
-        if ($this->client_os == "huggle-devs")
-            return false;
-        return version_compare($this->client_version, $this->getNewVersion(), '<');
-    }
-
-    function getNewVersion()
-    {
-        // here can you change the logic to return the latest version based on e.g. os
-        if ($this->beta) {
-            return self::LatestBeta();
-        } else {
-            return self::Latest();
-        }
-    }
-
-    function __construct()
-    {
-        if (!isset($_GET['os'])) {
-            $this->setError("System must be defined");
-            return;
-        }
-        if (!isset($_GET['version'])) {
-            $this->setError("Version must be defined");
-            return;
-        }
-        $this->client_version = preg_replace('/[^a-zA-Z0-9-_\.]/', '', $_GET['version']);
-        $this->client_os = preg_replace('/[^a-zA-Z0-9-_\.]/', '', $_GET['os']);
-
-        if (isset($_GET['notifybeta'])) {
-            $this->beta = true;
-        }
-    }
-}
+require("client.php");
 
 $c = new client();
+
+// we log these for statistics so that we have some unreliable information regarding
+// huggle usage :)
+$ip = "";
+if (isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+else
+    $ip = $_SERVER['REMOTE_ADDR'];
+$log = date('m/d/Y h:i:s a', time()) . ": update check, IP hash: " . md5($ip) . " version: " . $c->client_version . " os: " . $c->client_os . "\n";
+file_put_contents("updates.txt", $log, FILE_APPEND);
 
 header("Content-type: text/xml");
 echo "<?xml version=\"1.0\"?>\n";
