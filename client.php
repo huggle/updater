c5365ba3491bba5142e7cdd11a4c252d664b4425
Petr Bena <benapetr@gmail.com>
2015-06-25 14:56:14 +0200

    let's push latest version to public

--- client.php
+++ client.php
@@ -21,13 +21,13 @@ class client
 
     public static function Latest()
     {
-        return "3.1.12";
+        return "3.1.13";
     }
 
     public static function LatestMac()
     {
     	// return self::Latest();
-    	return "3.1.12";
+    	return "3.1.13";
     }
 
     public static function LatestBeta()
