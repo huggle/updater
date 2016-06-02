c7b91dddbe66a8f0501b753a6ef8f9178c5b070a
Petr Bena <benapetr@gmail.com>
2016-06-02 19:59:42 +0200

    new versions

--- client.php
+++ client.php
@@ -21,13 +21,13 @@ class client
 
     public static function Latest()
     {
-        return "3.1.19";
+        return "3.1.21";
     }
 
     public static function LatestMac()
     {
     	// return self::Latest();
-    	return "3.1.18";
+    	return "3.1.20";
     }
 
     public static function LatestBeta()
